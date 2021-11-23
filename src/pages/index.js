e3142a9a53caa9cce86913b55d79d5ef0c4d87b4
ricky-99 <ricky.99@tamu.edu>
2021-11-22 19:26:07 -0600

    Format first page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,22 @@
-import React from "react"
+//import React from "react"
+//import {Card} from '@material-ui/core/Button'
+
+import * as React from 'react';
+import {Paper} from '@mui/material';
+import centered from './styles.css'
+
 
 export default function Home() {
-  return <div>Hello world! Test</div>
+  return (
+    <div style={{ backgroundColor: 'maroon', width: '100%', height: '800px'}}>\
+      <div class="centered">
+        <Paper sx= {{ width: 500, height: 500}} elevation= '18'>
+            <div class="centered">
+              Add first form here
+            
+            </div>
+        </Paper>
+      </div>
+    </div>
+  )
 }
